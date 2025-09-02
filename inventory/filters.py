3d34324d7beb084daa0fea9b75639e46ad9b91d2
Renan Rivera <renan.r@shore360.com>
2025-09-02 14:17:54 +0800

    fix filter query on site, member, devision

--- inventory/filters.py
+++ inventory/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from django_filters import DateFilter, CharFilter, ChoiceFilter
 from .models import Item, ItemBase, Site, Floor, TeamMember, DemandItems, Division
+from django_filters import ModelChoiceFilter
 from django import forms
 
 
@@ -9,30 +10,6 @@ remarks_select = (
     ('OUT', 'OUT'),
     ('BEGINNING','BEGINNING')
 )
-
-
-# #department list
-# departments = Department.objects.all()
-# departments_list = []
-
-# #get department values
-# for value in departments:
-#     departments_list.append((value.id, value.department))
-
-#Site List
-site = Site.objects.all()
-site_list = []
-
-for value in site:
-    site_list.append((value.id, value.site))
-
-
-# #Floor List
-# floor = Floor.objects.all()
-# floor_list = []
-
-# for value in floor:
-#     floor_list.append((value.id, value.floor))
 
 
 # Unique Floor List based only on the 'floor' field
@@ -47,32 +24,6 @@ for value in floor:
         floor_list.append((value.id, value.floor))  # Append the original data to the list
 
 
-
-#Member List
-member = TeamMember.objects.all()
-member_list = []
-
-for value in member:
-    member_list.append((value.id, value.member))
-
-
- #Demand Item List
-demand_item = DemandItems.objects.all()
-demand_item_list = []
-
-for value in demand_item:
-    demand_item_list.append((value.id, value.demand_item))
-
-#Division List
-division = Division.objects.all()
-division_list = []
-
-for value in division:
-    division_list.append((value.id, value.division))
-
-
-
-
 class DateInput(forms.DateInput):
     input_type = 'date'
 
@@ -83,12 +34,10 @@ class ItemFilter(django_filters.FilterSet):
     remarks = ChoiceFilter(field_name='remarks', label="REMARKS", choices=remarks_select)
     date_from = DateFilter(field_name='date_added', lookup_expr='date__gte', label="DATE FROM", widget=DateInput(attrs={'type': 'date'}))
     date_to = DateFilter(field_name='date_added', lookup_expr='date__lte', label="DATE TO", widget=DateInput(attrs={'type': 'date'}))
-    member = ChoiceFilter(field_name='member', label="STAFF NAME", choices=member_list)
-    # department= ChoiceFilter(field_name='department_name', label="DEPARTMENT", choices=departments_list)
-    # client= ChoiceFilter(field_name='client_name', label="CLIENT", choices=clients_list)
-    site = ChoiceFilter(field_name='site', label="SITE", choices=site_list)
+    member = ModelChoiceFilter(field_name='member', label="STAFF NAME", queryset=TeamMember.objects.all())
+    site = ModelChoiceFilter(field_name='site', label="SITE", queryset=Site.objects.all())
     floor = ChoiceFilter(field_name='floor', label="FLOOR", choices=floor_list)
-    division = ChoiceFilter(field_name='division', label="DIVISION", choices=division_list)
+    division = ModelChoiceFilter(field_name='division', label="DIVISION", queryset=Division.objects.all())
 
    
     class Meta:
@@ -97,13 +46,13 @@ class ItemFilter(django_filters.FilterSet):
 
 
 class ItemBaseFilter(django_filters.FilterSet):
-    site = ChoiceFilter(field_name='site', label="SITE", choices=site_list)
+    site = ModelChoiceFilter(field_name='site', label="SITE", queryset=Site.objects.all())
     item_name = CharFilter(field_name='item_name', lookup_expr='icontains', label="ITEM NAME")
     brand_name = CharFilter(field_name='brand_name', lookup_expr='icontains', label="BRAND NAME")
     date_from = DateFilter(field_name='date_added', lookup_expr='date__gte', label="DATE FROM", widget=DateInput(attrs={'type': 'date'}))
     date_to = DateFilter(field_name='date_added', lookup_expr='date__lte', label="DATE TO", widget=DateInput(attrs={'type': 'date'}))
-    demand_item = ChoiceFilter(field_name='demand_item', label="DEMAND ITEM", choices=demand_item_list)
-    division = ChoiceFilter(field_name='division', label="DIVISION", choices=division_list)
+    demand_item = ModelChoiceFilter(field_name='demand_item', label="DEMAND ITEM", queryset=DemandItems.objects.all())
+    division = ModelChoiceFilter(field_name='division', label="DIVISION", queryset=Division.objects.all())
 
     class Meta:
         model = ItemBase
